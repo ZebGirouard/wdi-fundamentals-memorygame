93eb61ee983ea584b584a9112abb68133fa0d1b7
ZebGirouard <timzebgir@gmail.com>
2016-10-06 12:19:55 -0600

    adding game logic

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,1 +1,6 @@
-console.log("JS file is connected to HTML! Woo!")
+console.log("JS file is connected to HTML! Woo!");
+//Making four cards
+var cardOne = "king";
+var cardTwo = "queen";
+var cardThree = "king";
+var cardFour = "queen";
