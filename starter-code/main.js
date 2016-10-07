9dc5f9ffc059f8b6470789134b6c302fb93150b3
ZebGirouard <timzebgir@gmail.com>
2016-10-07 09:57:48 -0600

    First alerts

--- starter-code/main.js
+++ starter-code/main.js
@@ -4,3 +4,13 @@ var cardOne = "king";
 var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "queen";
+
+var checkOne = cardOne;
+var checkTwo = cardTwo;
+
+if (cardOne === cardThree) {
+	alert("You've found a match!");
+}
+else {
+	alert("Sorry, try again!");
+}
